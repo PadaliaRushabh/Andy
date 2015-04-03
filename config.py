f22816f1ac7b1d07264247e49564239453c1372c
Shivang Desai <shivang.ice.2010@gmail.com>
2015-04-04 03:40:18 +0530

    Create config.py
    Global config file for Andy

---
+++ config.py
@@ -0,0 +1,15 @@
+'''
+Global variables definition
+
+@author : Shivang
+'''
+
+keystore_name = "Andy.keystore"
+alias = "Andy-Signer"
+pwd = "password123" # Password can be changed here.
+name = "Andy-Signer"
+org_unit = "AndyOrgUnit"
+org = "AndyOrg"
+city = "AndyCity"
+state = "AndyState"
+country_code = "AS"
